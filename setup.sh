a7ebf0ddcb43c29821d547050de9ed0ccfc1e58c
Ross Masters <ross@rossmasters.com>
2013-09-20 23:51:03 +0100

    dist-upgrade on setup, fixes #2

--- setup.sh
+++ setup.sh
@@ -4,5 +4,7 @@ sudo apt-get -y install python-software-properties
 sudo add-apt-repository -y ppa:rquillo/ansible
 sudo apt-get -y install ansible
 
+sudo apt-get dist-upgrade -yq
+
 echo localhost > /etc/ansible/hosts
 sudo ansible-playbook /ansible/main.yml --connection=local
